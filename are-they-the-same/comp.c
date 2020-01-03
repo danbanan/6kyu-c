c59232df32d4cb96f93b605cfaf8720e76b1e742
Dan Rachou <dan.rachou@gmail.com>
2020-01-03 18:48:25 +0100

    Passes all the tests
    1. Sort the two arrays
    2. Compare the arrays
    
    I think this is faster than two nested for-loops.

--- are-they-the-same/comp.c
+++ are-they-the-same/comp.c
@@ -1,21 +1,29 @@
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
+int compare_ints(const void* a, const void* b)
+{
+  const int arg_a= *(const int*)a;
+  const int arg_b = *(const int*)b;
+
+  return arg_a - arg_b;
+}
 
 bool comp(const int *a, const int *b, size_t n)
 {
-  int sum1 = 0;
-  int sum2 = 0;
   int arr_a[n];
   int arr_b[n];
 
   memcpy(arr_a, a, n * sizeof(int));
   memcpy(arr_b, b, n * sizeof(int));
 
-  for (int i = 0; i < n; i++)
-    {
-      sum1 += arr_a[i] * arr_a[i];
-      sum2 += arr_b[i];
-    }
+  qsort(arr_a, n, sizeof(int), compare_ints);
+  qsort(arr_b, n, sizeof(int), compare_ints);
 
-  return sum1 == sum2;
+  for (int i = 0; i < n; i++)
+    if (arr_b[i] - (arr_a[i] * arr_a[i]))
+      return false;
+
+  return true;
 }
