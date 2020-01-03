4928e2b096c116004be8da5d0cf4a83a5c23ed09
Dan Rachou <dan.rachou@gmail.com>
2020-01-03 19:58:31 +0100

    Changed name on helper function

--- are-they-the-same/comp.c
+++ are-they-the-same/comp.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int compare_ints(const void* a, const void* b)
+int comp_ints(const void* a, const void* b)
 {
   const int arg_a= *(const int*)a;
   const int arg_b = *(const int*)b;
@@ -18,8 +18,8 @@ bool comp(const int *a, const int *b, size_t n)
   memcpy(arr_a, a, n * sizeof(int));
   memcpy(arr_b, b, n * sizeof(int));
 
-  qsort(arr_a, n, sizeof(int), compare_ints);
-  qsort(arr_b, n, sizeof(int), compare_ints);
+  qsort(arr_a, n, sizeof(int), comp_ints);
+  qsort(arr_b, n, sizeof(int), comp_ints);
 
   for (int i = 0; i < n; i++)
     if (arr_b[i] - (arr_a[i] * arr_a[i]))
